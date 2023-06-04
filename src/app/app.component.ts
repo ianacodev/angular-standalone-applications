c2ca373ce096bc0e190794d74d60783ab10c0ecd
ianacodev <ianaco.dev@gmail.com>
2023-06-04 16:56:26 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 // components
 import { ShopComponent } from './shop/shop.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, ShopComponent],
+  imports: [RouterModule, ShopComponent],
   selector: 'store-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
